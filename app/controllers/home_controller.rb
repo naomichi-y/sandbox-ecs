b568715fc2d6b81c5a635e6cc07c39ee76634486
naomichi-y <n.yamakita@gmail.com>
2017-04-02 01:45:19 +0900

    Refactor

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
-  def index
+  def index; end
+
+  def raise_error
+    raise 'Invoke raise error'
+  end
+
+  def logger_error
+    logger.error 'Invoke logger error'
+    head 200
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root :to => 'home#index'
+  root to: 'home#index'
+  get 'raise_error', to: 'home#raise_error'
+  get 'logger_error', to: 'home#logger_error'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
