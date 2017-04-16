1e861665f9f66664eaf9e7ac1d8256e3137eabab
naomichi-y <n.yamakita@gmail.com>
2017-04-16 23:05:29 +0900

    Add 404 page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def render_404(exception = nil)
+    STDERR.puts "error_404: #{exception.message}" if exception
+    @title = '404 Page Not Found'
+    render template: 'errors/error', status: 404, layout: 'application'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   get 'raise_error', to: 'home#raise_error'
   get 'logger_error', to: 'home#logger_error'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '*path', controller: 'application', action: 'render_404'
 end
