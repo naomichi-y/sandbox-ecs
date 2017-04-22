f0cc58169f39df03a8677ad0e8f9646158565a9f
naomichi-y <n.yamakita@gmail.com>
2017-04-23 06:00:34 +0900

    Refactor

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   get 'raise_error', to: 'home#raise_error'
   get 'logger_error', to: 'home#logger_error'
 
-  get '*path', controller: 'application', action: 'render_404'
+  match '*path', controller: 'application', action: 'render_404', via: :all
 end
