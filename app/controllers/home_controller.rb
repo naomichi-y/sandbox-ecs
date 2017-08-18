431a66c264df5e66eca60c3955cdd0882df9144c
naomichi-y <n.yamakita@gmail.com>
2017-08-19 03:41:11 +0900

    Fix typo

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,17 +6,17 @@ class HomeController < ApplicationController
   end
 
   def logger_warn
-    logger.error "LOGGER\nERROR"
-    head 200
-  end
-
-  def logger_error
     logger.error "LOGGER\nWARN"
     head 200
   end
 
+  def logger_error
+    logger.error "LOGGER\nERROR"
+    head 200
+  end
+
   def logger_fatal
-    logger.fatal "LOGGER\nERROR"
+    logger.fatal "LOGGER\nFATAL"
     head 200
   end
 end
